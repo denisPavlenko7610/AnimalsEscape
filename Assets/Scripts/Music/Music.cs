f7dea812c235bdc0a03c45392549e63a4839e065
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-06-12 12:16:56 +0300

    add background music

---
+++ Assets/Scripts/Music/Music.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using Random = UnityEngine.Random;
+
+namespace AnimalsEscape.Music
+{
+    public class Music : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer;
+        [SerializeField] private MusicSO _musicSo;
+        [SerializeField] private AudioSource _musicSource;
+        [SerializeField] private AudioSource _sfxSource;
+
+        private void Update()
+        {
+            if (!_musicSource.isPlaying)
+            {
+                PlaySource(_musicSource);
+            }
+        }
+
+        private void PlaySource(AudioSource audioSource)
+        {
+            audioSource.PlayOneShot(GetRandomMusic());
+        }
+
+        private AudioClip GetRandomMusic()
+        {
+            var number = SetRandomNumber(0, _musicSo.Music.Count);
+            return _musicSource.clip = _musicSo.Music[number];
+        }
+
+        private int SetRandomNumber(int minIndex, int maxIndex) => Random.Range(minIndex, maxIndex);
+    }
+}
---
+++ Assets/Scripts/Music/MusicSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnimalsEscape.Music
+{
+    [CreateAssetMenu(fileName = "MusicConfig", menuName = "Music/Create config", order = 0)]
+    public class MusicSO : ScriptableObject
+    {
+        public List<AudioClip> Music = new();
+        public List<AudioClip> MusicFX = new();
+    }
+}

10b9e57e0da1fb0142e598d1998d3e16b1cdff12
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-06-26 10:55:46 +0300

    add mute / unmute music

--- Assets/Scripts/Music/Music.cs
+++ Assets/Scripts/Music/Music.cs
@@ -1,35 +1,96 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 namespace AnimalsEscape.Music
 {
     public class Music : MonoBehaviour
     {
-        [SerializeField] private AudioMixer _audioMixer;
-        [SerializeField] private MusicSO _musicSo;
-        [SerializeField] private AudioSource _musicSource;
-        [SerializeField] private AudioSource _sfxSource;
+        [SerializeField] AudioMixer _audioMixer;
+        [SerializeField] MusicSO _musicSo;
+        [SerializeField] AudioSource _musicSource;
+        [SerializeField] AudioSource _sfxSource;
+        [SerializeField] Image _soundImage;
+        [SerializeField] Button _soundButton;
 
-        private void Update()
+        bool isStop;
+        const string key = "isStop";
+
+        private void OnEnable()
         {
-            if (!_musicSource.isPlaying)
+            LoadVolumeSettings();
+            _soundButton.onClick.AddListener(StopPlayingSound);
+        }
+
+        private void LoadVolumeSettings()
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                isStop = PlayerPrefs.GetInt(key) == 1;
+                SetSoundSettings();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _soundButton.onClick.RemoveListener(StopPlayingSound);
+        }
+
+        void Update()
+        {
+            CheckPlayMusic();
+        }
+
+        private void CheckPlayMusic()
+        {
+            if (!_musicSource.isPlaying && !isStop)
             {
                 PlaySource(_musicSource);
             }
         }
 
-        private void PlaySource(AudioSource audioSource)
+        void PlaySource(AudioSource audioSource)
         {
             audioSource.PlayOneShot(GetRandomMusic());
         }
 
-        private AudioClip GetRandomMusic()
+        void StopPlayingSound()
+        {
+            isStop = !isStop;
+            SetSoundSettings();
+        }
+
+        private void SetSoundSettings()
+        {
+            if (isStop)
+                MuteVolume();
+            else
+                UnmuteVolume();
+        }
+
+        private void UnmuteVolume()
+        {
+            PlayerPrefs.SetInt(key, 0);
+            var standardVolume = -11f;
+            _audioMixer.SetFloat("MasterVolume", standardVolume);
+            _soundImage.sprite = Resources.Load<Sprite>("Sound/VolumeUnmute");
+        }
+
+        private void MuteVolume()
+        {
+            PlayerPrefs.SetInt(key, 1);
+            var muteVolume = -80f;
+            _audioMixer.SetFloat("MasterVolume", muteVolume);
+            _soundImage.sprite = Resources.Load<Sprite>("Sound/VolumeMute");
+        }
+
+        AudioClip GetRandomMusic()
         {
             var number = SetRandomNumber(0, _musicSo.Music.Count);
             return _musicSource.clip = _musicSo.Music[number];
         }
 
-        private int SetRandomNumber(int minIndex, int maxIndex) => Random.Range(minIndex, maxIndex);
+        int SetRandomNumber(int minIndex, int maxIndex) => Random.Range(minIndex, maxIndex);
     }
 }
